f7b46b3b82e1940d7e42d2dfc356f389f066484c
Tretiak-Anna <tretiakann@gmail.com>
2024-04-13 12:00:22 +0300

    async function getAllRecipes

--- src/js/dummy-api.js
+++ src/js/dummy-api.js
@@ -1,6 +1,17 @@
 import axios from 'axios';
 
-export async function getAllRecipes() {
-  const { data } = await axios('https://dummyjson.com/recipes');
-  return data;
+const BASE_URL = 'https://dummyjson.com/recipes';
+
+export async function getAllRecipes(url, options = {}) {
+  const response = await axios(url, options);
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  return await response.json();
 }
+
+getAllRecipes(BASE_URL)
+  .then(recipes =>
+    container.insertAdjacentHTML('beforeend', createMarkup(recipes))
+  )
+  .catch(error => console.log(error));
