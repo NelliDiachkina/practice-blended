73cef78a632723ccf0bb700dc876a0853d0a5632
INNA-KUSHNIRR1 <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2024-04-19 22:27:12 +0200

    css for modal

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -23,14 +23,18 @@ export function renderModalRecipeMarkup(
   },
   element
 ) {
-  const markup = `<div>
-  <img src="${image}" alt="${name}" />
-  <div>
-    <h3>Name: ${name}</h3>
-    <p>Ingredints:</p>
-    <ul>${ingredients.map(el => `<li>${el}</li>`).join(``)}</ul>
-    <p>Instructions:</p>
-    <ul>${instructions.map(el => `<li>${el}</li>`).join(``)}</ul>
+  const markup = `<div class="modal">
+  <div class="modal-img"><img src="${image}" alt="${name}" /></div>
+  <div class="modal-description">
+    <h3 class="modal-title">Name: ${name}</h3>
+    <p class="modal-chapter">Ingredients:</p>
+    <ul class="modal-chapter-text">${ingredients
+      .map(el => `<li>${el}</li>`)
+      .join(``)}</ul>
+    <p class="modal-chapter">Instructions:</p>
+    <ul class="modal-chapter-text">${instructions
+      .map(el => `<li>${el}</li>`)
+      .join(``)}</ul>
     <span> Cook Time: ${cookTimeMinutes} </span>
     <span>Servings: ${servings}</span>
     <span>Difficulty: ${difficulty}</span>
